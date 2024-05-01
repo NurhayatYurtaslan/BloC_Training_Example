753db73adc4fa72c7be1a21ef10752193a36824a
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-01 22:27:08 +0300

    update 🆕: main file

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:counter_app/app/view/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main () => runApp(const CounterApp());
+void main() => runApp(const CounterApp());
 
-class CounterApp extends StatelessWidget { //Fonksiyon kullanmıyorsun bu yüzden StatelessWidget
+class CounterApp extends StatelessWidget {
+  //Fonksiyon kullanmıyorsun bu yüzden StatelessWidget
   const CounterApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return const MaterialApp(
-      home:HomeView(),
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
     );
   }
 }
